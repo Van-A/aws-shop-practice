f29b339b4134b7dade0207e1c593844461f6e96a
Andrei Vaniushyn <andrei_vaniushyn@epam.com>
2023-03-05 21:19:51 +0300

    task-4 Integration with NoSQL Database

--- product-service/getProductsList.js
+++ product-service/getProductsList.js
@@ -1,9 +1,30 @@
-'use strict';
-const productsMock = require("./productsMock");
+const AWS = require("aws-sdk");
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+const scan = async (tableName) => {
+  const scanResults = await dynamo
+    .scan({
+      TableName: tableName,
+    })
+    .promise();
+  return scanResults.Items;
+};
 
 module.exports = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(productsMock),
-    };
+  const products = await scan(process.env.PRODUCTS_TABLE_NAME);
+  const stocks = await scan(process.env.STOCKS_TABLE_NAME);
+
+  const stocksMap = stocks.reduce((acc, stock) => {
+    acc[stock.product_id] = stock.count;
+    return acc;
+  }, {});
+  const productsAndCount = products.map((product) => ({
+    ...product,
+    count: stocksMap[product.id],
+  }));
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(productsAndCount),
   };
+};
